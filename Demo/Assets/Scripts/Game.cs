eb8c9083c044fd98b81580b28eb4aa95cc2172ab
DESKTOP-5S2UP63\admin <xtu@zju.edu.cn>
2021-06-16 17:43:11 +0800

    更新到4.多场景加载

--- Demo/Assets/Scripts/Game.cs
+++ Demo/Assets/Scripts/Game.cs
@@ -1,28 +1,57 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Game : PersistableObject {
 
-	const int saveVersion = 1;
+	const int saveVersion = 2;
 
 	public ShapeFactory shapeFactory;
 
 	public KeyCode createKey = KeyCode.C;
+	public KeyCode destroyKey = KeyCode.X;
 	public KeyCode newGameKey = KeyCode.N;
 	public KeyCode saveKey = KeyCode.S;
 	public KeyCode loadKey = KeyCode.L;
 
 	public PersistentStorage storage;
 
+	public int levelCount;
+
+	public float CreationSpeed { get; set; }
+
+	public float DestructionSpeed { get; set; }
+
 	List<Shape> shapes;
 
-	void Awake () {
+	float creationProgress, destructionProgress;
+
+	int loadedLevelBuildIndex;
+
+	void Start () {
 		shapes = new List<Shape>();
+
+		if (Application.isEditor) {
+			for (int i = 0; i < SceneManager.sceneCount; i++) {
+				Scene loadedScene = SceneManager.GetSceneAt(i);
+				if (loadedScene.name.Contains("Level ")) {
+					SceneManager.SetActiveScene(loadedScene);
+					loadedLevelBuildIndex = loadedScene.buildIndex;
+					return;
+				}
+			}
+		}
+
+		StartCoroutine(LoadLevel(1));
 	}
 
 	void Update () {
 		if (Input.GetKeyDown(createKey)) {
 			CreateShape();
+		}
+		else if (Input.GetKeyDown(destroyKey)) {
+			DestroyShape();
 		}
 		else if (Input.GetKeyDown(newGameKey)) {
 			BeginNewGame();
@@ -34,13 +63,49 @@ public class Game : PersistableObject {
 			BeginNewGame();
 			storage.Load(this);
 		}
+		else {
+			for (int i = 1; i <= levelCount; i++) {
+				if (Input.GetKeyDown(KeyCode.Alpha0 + i)) {
+					BeginNewGame();
+					StartCoroutine(LoadLevel(i));
+					return;
+				}
+			}
+		}
+
+		creationProgress += Time.deltaTime * CreationSpeed;
+		while (creationProgress >= 1f) {
+			creationProgress -= 1f;
+			CreateShape();
+		}
+
+		destructionProgress += Time.deltaTime * DestructionSpeed;
+		while (destructionProgress >= 1f) {
+			destructionProgress -= 1f;
+			DestroyShape();
+		}
 	}
 
 	void BeginNewGame () {
 		for (int i = 0; i < shapes.Count; i++) {
-			Destroy(shapes[i].gameObject);
+			shapeFactory.Reclaim(shapes[i]);
 		}
 		shapes.Clear();
+	}
+
+	IEnumerator LoadLevel (int levelBuildIndex) {
+		enabled = false;
+		if (loadedLevelBuildIndex > 0) {
+			yield return SceneManager.UnloadSceneAsync(loadedLevelBuildIndex);
+		}
+		yield return SceneManager.LoadSceneAsync(
+			levelBuildIndex, LoadSceneMode.Additive
+		);
+		SceneManager.SetActiveScene(
+			SceneManager.GetSceneByBuildIndex(levelBuildIndex)
+		);
+		loadedLevelBuildIndex = levelBuildIndex;
+		enabled = true;
 	}
 
 	void CreateShape () {
@@ -58,8 +123,19 @@ public class Game : PersistableObject {
 		shapes.Add(instance);
 	}
 
+	void DestroyShape () {
+		if (shapes.Count > 0) {
+			int index = Random.Range(0, shapes.Count);
+			shapeFactory.Reclaim(shapes[index]);
+			int lastIndex = shapes.Count - 1;
+			shapes[index] = shapes[lastIndex];
+			shapes.RemoveAt(lastIndex);
+		}
+	}
+
 	public override void Save (GameDataWriter writer) {
 		writer.Write(shapes.Count);
+		writer.Write(loadedLevelBuildIndex);
 		for (int i = 0; i < shapes.Count; i++) {
 			writer.Write(shapes[i].ShapeId);
 			writer.Write(shapes[i].MaterialId);
@@ -74,6 +150,7 @@ public class Game : PersistableObject {
 			return;
 		}
 		int count = version <= 0 ? -version : reader.ReadInt();
+		StartCoroutine(LoadLevel(version < 2 ? 1 : reader.ReadInt()));
 		for (int i = 0; i < count; i++) {
 			int shapeId = version > 0 ? reader.ReadInt() : 0;
 			int materialId = version > 0 ? reader.ReadInt() : 0;
